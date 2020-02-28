e63a83a8ea8eb5e95ea490d24a975032796e166f
Carlos Garcia <gtxcarl9@gmail.com>
2020-02-28 23:38:20 +0100

    Criado o ficheiro  do describle

---
+++ src/components/Home/Describle.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Describle.css'
+function Describle(){
+    return(
+        <main className="describle">
+            <div className="describle-container">
+                <h1>Água AKI está revolucionando as entregas urgentes!</h1>
+                <p>
+                    A Click Entregas é uma empresa que tem uma plataforma online de entregas expressas, que conecta os clientes que precisam fazer uma entrega rápida a um entregador disponível na região.
+                </p>
+                <p>
+                    Se você ou sua empresa precisarem fazer qualquer tipo de entrega rápida, não esqueça da gente. É super simples! Você faz todo o processo através do nosso site: clickentregas.com
+                </p>
+                <p>
+                    Estamos presentes em 11 países: Brasil, Rússia, Índia, México, Coreia do Sul, Indonésia, Turquia, Malásia, Vietnã, Filipinas e Tailândia!
+                </p>
+                <p>
+                    A Click Entregas é um parceiro de entrega confiável para lojas on-line e restaurantes. Nossos serviços mais populares são entregas urgentes de documentos, compras on-line, flores, presentes e mercadorias; até 20kg por moto.
+                </p>
+                <button>Agende uma encomenda</button>
+            </div>
+        </main>
+    )
+}
+
+export default Describle
