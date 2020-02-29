80b4f9c21c0c21f3e49d447971d0c3c0ac38b0ee
Carlos Garcia <gtxcarl9@gmail.com>
2020-02-29 01:56:31 +0100

    Remoção do loron ipsun

--- src/components shared/Header.js
+++ src/components shared/Header.js
@@ -4,12 +4,12 @@ function Header(){
     return(
         <header>
             <div className="header-container">
-                <h1>My logo</h1>
+                <h1>Água Ki</h1>
                 <ul>
-                    <li className="call-me">Lorem ipsum dolor</li>
-                    <li className="number">+1 <span> 333666777</span> </li>
-                    <li className="btn-solicitar">Lorem ipsum </li>
-                    <li className="btn-sign-in">Consectetur adipi</li>
+                    <li className="call-me">Ligue para mim</li>
+                    <li className="number">+244 <span> 914682536</span> </li>
+                    <li className="btn-solicitar">Solicitar entrega</li>
+                    <li className="btn-sign-in">Entrar / Cadastrar</li>
                 </ul>
             </div>
         </header>
