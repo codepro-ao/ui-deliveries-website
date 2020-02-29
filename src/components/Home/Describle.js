98481a457322b0cea0d83c5563d3c56cf3bbafd8
Carlos Garcia <gtxcarl9@gmail.com>
2020-02-29 01:57:41 +0100

    Remoção do loom ipsum

--- src/components/Home/Describle.js
+++ src/components/Home/Describle.js
@@ -4,20 +4,20 @@ function Describle(){
     return(
         <main className="describle">
             <div className="describle-container">
-                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing .</h1>
+                <h1>Água AKI está revolucionando as entregas urgentes!</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel erat felis. Morbi lobortis diam sit amet egestas elementum. Cras fringilla dictum porttitor. Praesent libero odio.
+                    A Click Entregas é uma empresa que tem uma plataforma online de entregas expressas, que conecta os clientes que precisam fazer uma entrega rápida a um entregador disponível na região.
                 </p>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel erat felis. Morbi lobortis diam sit amet egestas elementum. Cras fringilla dictum porttitor raesent.
+                    Se você ou sua empresa precisarem fazer qualquer tipo de entrega rápida, não esqueça da gente. É super simples! Você faz todo o processo através do nosso site: clickentregas.com
                 </p>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel erat felis. Morbi lobortis diam sit amet egestas elementum. Cras fringilla dictum porttitor. 
+                    Estamos presentes em 11 países: Brasil, Rússia, Índia, México, Coreia do Sul, Indonésia, Turquia, Malásia, Vietnã, Filipinas e Tailândia!
                 </p>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel erat felis. Morbi lobortis diam sit amet egestas elementum. Cras fringilla dictum porttitor. Praesent libero odio, mattis pulvinar orci non, dapibus porta nunc. 
+                    A Click Entregas é um parceiro de entrega confiável para lojas on-line e restaurantes. Nossos serviços mais populares são entregas urgentes de documentos, compras on-line, flores, presentes e mercadorias; até 20kg por moto.
                 </p>
-                <button>Lorem ipsum dolor sit</button>
+                <button>Agende uma encomenda</button>
             </div>
         </main>
     )
