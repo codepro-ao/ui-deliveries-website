8cec7a3e055aec7718da9dda62c5d5ffe2ad1d1b
Carlos Garcia <gtxcarl9@gmail.com>
2020-03-15 00:32:34 +0100

    Added  btn  on header

--- src/components shared/Header.js
+++ src/components shared/Header.js
@@ -1,19 +1,49 @@
-import React from 'react'
+import React,{Component} from 'react'
 import './Header.css'
-function Header(){
-    return(
-        <header className="header">
-            <div className="header-container">
-                <h1>My logo</h1>
-                <ul>
-                    <li className="call-me">Lorem ipsum dolor</li>
-                    <li className="number">+1 <span> 333666777</span> </li>
-                    <li className="btn-solicitar">Lorem ipsum </li>
-                    <li className="btn-sign-in">Consectetur adipi</li>
-                </ul>
-            </div>
-        </header>
-    )
-}
 
-export default Header
+export default class Header extends Component{
+
+    constructor(){
+        super();
+        this.state={
+                visFlex:'flex',
+                alterPos:'rotate(180deg)'
+        } 
+    }
+    toggleBtn(){
+        if(this.state.visFlex==='flex'){
+            this.setState({
+                visFlex:'none',
+                alterPos:'rotate(0)'
+            });
+        }
+        else{
+            this.setState({
+                visFlex:'flex',
+                alterPos:'rotate(180deg)'
+            });
+        }
+        document.querySelector(".menu-container-mobile").style.display=this.state.visFlex;
+        document.querySelector(".mobile-menu").style.transform=this.state.alterPos;
+    }
+    render(props ){
+        return(
+            <header className="header">
+                <div className="header-container">
+                    <h1>My logo</h1>
+                    <ul className="">
+                        <li className="call-me">Lorem ipsum dolor</li>
+                        <li className="number">+1 <span> 333666777</span> </li>
+                        <li className="btn-solicitar">Lorem ipsum </li>
+                        <li className="btn-sign-in">Consectetur adipi</li>
+                    </ul>
+                    <div className="menu-container-mobile">
+                        <button className="btn-solicitar">Lorem ipsum dolor</button>
+                    </div>
+                    <div  onClick={this.toggleBtn.bind(this)}   className="mobile-menu">
+                    </div>
+                </div>
+            </header>
+        );
+    }
+}
